689435f9733afaf696c2e60ee4d528b33741e067
Marcelo Barros <mhbarros99@gmail.com>
2025-12-27 17:09:53 -0300

    feat: sentry config

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
-import { SentryModule } from '@sentry/nestjs/setup'
+import { SentryGlobalFilter, SentryModule } from '@sentry/nestjs/setup'
+import { SwaggerModule } from '@nestjs/swagger'
+import { APP_FILTER } from '@nestjs/core'
 
 @Module({
-  imports: [SentryModule.forRoot()],
+  imports: [SentryModule.forRoot(), SwaggerModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [{ provide: APP_FILTER, useClass: SentryGlobalFilter }, AppService],
 })
 export class AppModule {}
--- apps/api/src/instrument.ts
+++ apps/api/src/instrument.ts
@@ -1,10 +1,9 @@
-import Sentry from '@sentry/nestjs'
-import { nodeProfilingIntegration } from '@sentry/profiling-node'
-
-const SENTRY_DSN = 'https://00082982eecf4199b5585b261a613ffb@o4509067240407040.ingest.us.sentry.io/4510608541286400'
+import * as Sentry from '@sentry/nestjs'
 
 Sentry.init({
-  dsn: SENTRY_DSN,
-  integrations: [nodeProfilingIntegration()],
+  dsn: 'https://00082982eecf4199b5585b261a613ffb@o4509067240407040.ingest.us.sentry.io/4510608541286400',
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
   sendDefaultPii: true,
+  release: '1.0.0',
 })
